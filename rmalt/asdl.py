c42611825d735e6388f9cc42dda13004d4474e24
thautwarm <twshere@outlook.com>
2018-06-21 14:23:56 +0800

    done

---
+++ rmalt/asdl.py
@@ -0,0 +1,77 @@
+from typing import Optional, Tuple
+
+from Redy.Magic.Classic import record
+from Redy.ADT.Core import data
+from Redy.ADT import traits
+
+
+
+
+class ASDL:
+
+    @data
+    class Const(traits.Im, traits.ConsInd, traits.Dense):
+        num: lambda _: str(_)
+        str: lambda _: _
+        true: ...
+        false: ...
+        nil: ...
+
+
+    @record
+    class Include:
+        path: str
+
+    @record
+    class Assign:
+        target: str
+        expr: ...
+
+    @record
+    class Cond:
+        maps: Optional[Tuple['ASDL', ...]]
+
+    @record
+    class Block:
+        stmts: Optional[Tuple['ASDL', ...]]
+
+    @record
+    class Symbol:
+        name: str
+
+    @record
+    class Map:
+        case: ...
+        do: ...
+
+
+    @record
+    class Call:
+        fn: ...
+        arg: ...
+
+    @record
+    class Tuple:
+        args: Optional[Tuple['ASDL', ...]]
+
+
+    @record
+    class List:
+        args: Optional[Tuple['ASDL', ...]]
+
+    @record
+    class Pair:
+        key: ...
+        value: ...
+
+    @record
+    class Dict:
+        args: Optional[Tuple['ASDL.Pair', ...]]
+
+    @record
+    class Lambda:
+        params: Optional[Tuple['ASDL', ...]]
+        body: ...
+
+
+
---
+++ test.py
@@ -0,0 +1,20 @@
+from Redy.Tools.PathLib import Path
+import rbnf.zero as ze
+
+with Path('./rmalt').into('malt.rbnf').open('r') as f:
+    rbnf = f.read()
+ze_exp = ze.compile(rbnf, use='Grammar')
+
+x = ze_exp.match("""
+  let x = "123" 
+| let y = 2
+| {1: 2}
+| cond{
+      x => 1
+    | y => 10
+    | z => 10
+    }
+""")
+print(x.result)
+
+
